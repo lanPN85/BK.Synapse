baf19fdcb400a3ba4ef11edc60540bbe7767b9bf
= <phan.ngoclan58@gmail.com>
2019-03-22 16:29:19 +0700

    More endpoints

--- bkdigits-api/apis/jobs/submit.py
+++ bkdigits-api/apis/jobs/submit.py
@@ -1,27 +1,82 @@
 import flask
 import os
+import signal
 
 from flask import current_app
 from flask_restful import Resource
+from subprocess import Popen
 
 from apis import utils
 from apis.logs import err_logged
-from bkdigits.jobs.model import TrainingJobConfig, TrainingJobConfigSchema
+from bkdigits.jobs.model import TrainingJobConfig, TrainingJobConfigSchema, TrainingJob, TrainingJobMetadata
 
 
 class SubmitTrainingJob(Resource):
     @err_logged
     def post(self):
         payload = flask.request.get_json()
-        payload = TrainingJobConfigSchema().load(payload).data
+        try:
+            train_config = TrainingJobConfigSchema().load(payload).data
+        except:
+            return {
+                'msg': 'Invalid config'
+            }, 400
 
-        dataset_path = utils.resolve_dataset_path(payload.dataset)
-        if not os.path.exists(dataset_path):
-            return {
-                'msg': 'Invalid dataset'
-            }, 400
-        val_dataset_path = utils.resolve_dataset_path(payload.valDataset)
-        if not os.path.exists(val_dataset_path):
-            return {
-                'msg': 'Invalid validation dataset'
-            }, 400
+        meta = TrainingJobMetadata()
+        job = TrainingJob.from_config(config, meta)
+        job.save()
+
+        return {
+            'msg': 'Success',
+            'job': {
+                'id': job.id
+            }
+        }
+
+
+class StartTrainingJob(Resource):
+    @err_logged
+    def post(self):
+        payload = flask.request.get_json()
+        job_id = payload['job']['id']
+
+        job = TrainingJob.load(job_id)
+
+        # Spawn horovod subprocess
+        host_list = ['localhost:1']
+        for node in job.config.nodes:
+            # TODO resolve node adressess
+            pass
+        host_str = ','.join(host_list)
+        workers = 1
+        worker_exc = os.path.join(
+            current_app.config['BACKEND_WORKER_DIRS'][job.config.backend],
+            'train.py')
+        
+        cmd = [
+            'mpirun', '-np', str(workers), '-H', host_str,
+            worker_exc, '--job-id', job.id
+        ]
+        proc = Popen(cmd, shell=True)
+        job.meta.pid = proc.pid
+        job.save()
+
+        return {
+            'msg': 'Success'
+        }
+
+
+class StopTrainingJob(Resource):
+    @err_logged
+    def post(self):
+        payload = flask.request.get_json()
+        job_id = payload['job']['id']
+
+        job = TrainingJob.load(job_id)
+        if job.meta.pid is not None:
+            os.kill(job.meta.pid, signal.SIGTERM)
+            os.waitpid(job.meta.pid)
+
+        return {
+            'msg': 'Success'
+        }
--- bkdigits-api/apis/utils.py
+++ bkdigits-api/apis/utils.py
@@ -1,2 +1,11 @@
 import os
+import zipfile
 
+
+def unzip(zip_path, target_dir=None):
+    if target_dir is None:
+        target_dir = os.path.dirname(zip_path)
+    
+    zf = zipfile.ZipFile(zip_path, mode='r')
+    zf.extractall(target_dir)
+    zf.close()
