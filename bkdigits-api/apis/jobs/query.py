6db57f5f38db88998aa53ff632f357dd07f1625f
= <phan.ngoclan58@gmail.com>
2019-03-25 14:43:37 +0700

    Sort jobs by created date

--- bkdigits-api/apis/jobs/query.py
+++ bkdigits-api/apis/jobs/query.py
@@ -23,7 +23,7 @@ class ListJobs(Resource):
 
         ids = os.listdir(ROOT_DIR)
         ids = filter(lambda x: os.path.isdir(os.path.join(ROOT_DIR, x)), ids)
-        ids = sorted(ids)
+        ids = sorted(ids, key=lambda x: x.meta.createdAt, reverse=True)
         if id_only:
             return {
                 'jobs': ids
