f69ae01074b3f99f95c0a71a75be29ed3d70c64c
lanpn <phan.ngoclan58@gmail.com>
2019-04-02 12:08:06 +0700

    Fix bad extract

--- bksynapse-api/apis/jobs/submit.py
+++ bksynapse-api/apis/jobs/submit.py
@@ -77,7 +77,7 @@ class StartTrainingJob(Resource):
         db_client = NodeDbClient.from_env()
         host_list = ['localhost:1']
         for node_conf in job.config.nodes:
-            node_id = node_conf.id
+            node_id = node_conf['id']
             node = db_client.get_node_by_id(node_id)
             if node is None:
                 continue
