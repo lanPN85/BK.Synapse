30f2e1fe0c9a914ec9b0a91f9b62d46f9137d719
lanpn <phan.ngoclan58@gmail.com>
2019-05-07 09:12:08 +0700

    Fix accepted files

--- bksynapse-web/src/components/models/CreateModel.vue
+++ bksynapse-web/src/components/models/CreateModel.vue
@@ -118,6 +118,7 @@ export default {
     weightFileDropOpts() {
       var opts = this.commonFileDropOpts()
       opts.maxFilesize = 1024
+      opts.acceptedFiles = '*'
       return opts
     },
     isValid() {
